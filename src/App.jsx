5546c16302a7b917a91988b5ee2abc0450f98bde
MD. Noruzzaman <tonmoyakand8@gmail.com>
2025-09-28 10:55:26 +0600

    create navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+import './App.css';
+import navImg from "./assets/logo.png";
+import dollarImg from "./assets/dollar-1.png";
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div class="navbar bg-base-100 shadow-sm">
-  <div class="flex-1">
-    <a class="btn btn-ghost text-xl">daisyUI</a>
-  </div>
-  <div class="flex-none">
-    <button class="btn btn-square btn-ghost">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" class="inline-block h-5 w-5 stroke-current"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"></path> </svg>
-    </button>
-  </div>
-</div>
+      <div class="navbar max-w-[1200px] mx-auto">
+        <div class="flex-1">
+          <a class="text-xl">
+            <img className='w-[60px] h-[60px]' src ={navImg} alt="" />
+          </a>
+        </div>
+        <div class="flex items-center">
+          <span className='mr-1' >60000000</span>
+          <span className='mr-1' >Coin</span>
+          <img src={dollarImg} alt=""/>
+        </div>
+      </div>
     </>
   )
 }
