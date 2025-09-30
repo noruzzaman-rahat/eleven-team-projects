97a638718ce9a923fcff99d1ed4bf479f2f9bf8d
MD. Noruzzaman <tonmoyakand8@gmail.com>
2025-09-30 19:18:20 +0600

    create button section and toggle button

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from "react";
+import { Suspense, useState } from "react";
 import "./App.css";
 
 import AvailablePlayers from "./components/AvailablePlayers/AvailablePlayers";
@@ -11,15 +11,28 @@ const fetchplayers = async () => {
 };
 
 function App() {
+
+  const [toggle, setToggle] = useState(true);
   const playersPromise = fetchplayers();
 
   return (
     <>
       <Navbar></Navbar>
-      <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
+      <div className="max-w-[1200px] mx-auto flex justify-between items-center">
+        <h1 className="font-bold text-2xl">Available players</h1>
+        <div>
+          <button onClick={()=>setToggle(true)} className={`py-3 px-4 border-1 border-gray-400 rounded-l-2xl border-r-0 ${toggle===true?"bg-[#E7FE29]":""}`}>Available</button>
+
+          <button onClick={()=>setToggle(false)} className={`py-3 px-4 border-1 border-gray-400 rounded-r-2xl border-l-0 ${toggle===false?"bg-[#E7FE29]":""}`}>Selected<span>0</span></button>
+        </div>
+      </div>
+      {
+        toggle === true ? <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
         <AvailablePlayers playersPromise={playersPromise}></AvailablePlayers>
-      </Suspense>
-      {/* <SelectedPlayers></SelectedPlayers> */}
+      </Suspense> : <SelectedPlayers></SelectedPlayers>
+      }
+      
+      
     </>
   );
 }
